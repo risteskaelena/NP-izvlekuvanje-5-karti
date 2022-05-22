276dd8644dcfaeb1c17dc264cea4d434c1d1693f
Elena <elena.risteska003@gmail.comgit config --global user.name Elenagit config --global user.email elena.risteska003@gmail.com>
2022-05-22 16:25:39 +0200

    dodavanje komentari

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
+//ElenaRisteskaINKI876
 #include <iostream>
 #include <stdlib.h>
-#include <time.h>
-#include <vector>
+#include <time.h> //vklucuvanje na biblioteka za vreme
+#include <vector> //vklucuvanje na biblioteka vektor
 
 using namespace std;
 
@@ -9,13 +10,16 @@ int main()
 {
     string karti [13]={"Kec","Dvojka","Trojka","Cetvorka","Petka","Sestka","Sedumka","Osumka","Devetka","Desetka","Dzandar","Kokona","Kral"};
     string tipKarti [4]= {"Detelina","List","Srce","Lokum"};
-    vector<string> spilKarti;
+    vector<string> spilKarti; //formiranje vektor od tip string
+    //ciklusi za redenje na spilot karti
     for(int i=0;i<4;i++) {
         for(int j=0;j<13;j++) {
             spilKarti.push_back(karti[j]+" "+tipKarti[i]);
         }
     }
+    //naredbi za mesanje na spilot karti
     srand(time(NULL));
+    //deklariranje promenlivi
     int rVrednost;
     string privremena;
     for(int i=0;i<52;i++) {
@@ -24,6 +28,7 @@ int main()
         spilKarti[i]=spilKarti[rVrednost];
         spilKarti[rVrednost]=privremena;
     }
+    //pecatenje na 5 karti
     for(int i=0;i<5;i++) {
         cout<<spilKarti[i]<<endl;
     }
