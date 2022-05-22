9420cecb85e4e487ecfb1298bb78a75e7bfc6543
Elena <elena.risteska003@gmail.comgit config --global user.name Elenagit config --global user.email elena.risteska003@gmail.com>
2022-05-22 16:10:25 +0200

    mesanje spil karti

--- main.cpp
+++ main.cpp
@@ -10,12 +10,20 @@ int main()
     string karti [13]={"Kec","Dvojka","Trojka","Cetvorka","Petka","Sestka","Sedumka","Osumka","Devetka","Desetka","Dzandar","Kokona","Kral"};
     string tipKarti [4]= {"Detelina","List","Srce","Lokum"};
     vector<string> spilKarti;
-    for(int i=0;i<4;i++)
-    {
-        for(int j=0;j<13;j++)
-        {
+    for(int i=0;i<4;i++) {
+        for(int j=0;j<13;j++) {
             spilKarti.push_back(karti[j]+" "+tipKarti[i]);
         }
     }
+    srand(time(NULL));
+    int rVrednost;
+    string privremena;
+    for(int i=0;i<52;i++) {
+        rVrednost=rand()%52;
+        privremena=spilKarti[i];
+        spilKarti[i]=spilKarti[rVrednost];
+        spilKarti[rVrednost]=privremena;
+    }
+    cout<<spilKarti[0]<<endl;
     return 0;
 }
