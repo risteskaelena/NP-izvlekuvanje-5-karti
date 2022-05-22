3c0392c673772aca7df29d4a52b00f457c46ee91
Elena <elena.risteska003@gmail.comgit config --global user.name Elenagit config --global user.email elena.risteska003@gmail.com>
2022-05-22 15:57:48 +0200

    formiranje spil karti

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    string karti [13]={"Kec","Dvojka","Trojka","Cetvorka","Petka","Sestka","Sedumka","Osumka","Devetka","Desetka","Dzandar","Kokona","Kral"};
+    string tipKarti [4]= {"Detelina","List","Srce","Lokum"};
+    vector<string> spilKarti;
+    for(int i=0;i<4;i++)
+    {
+        for(int j=0;j<13;j++)
+        {
+            spilKarti.push_back(karti[j]+" "+tipKarti[i]);
+        }
+    }
     return 0;
 }
